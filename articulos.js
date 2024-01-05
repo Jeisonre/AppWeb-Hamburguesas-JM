22660fcf1c6b5490af08c26eb03e7d2d770a4472
Jeisonre <jeisonespin25@gmail.com>
2024-01-05 00:42:41 -0500

    config

--- articulos.js
+++ articulos.js
@@ -2,7 +2,6 @@ function restar1(product1) {
     var inputNumero = document.getElementById("numeroProduct1");
     inputNumero.value = parseInt(inputNumero.value) - 1;
 }
-
 function sumar1(product1) {
     var inputNumero = document.getElementById("numeroProduct1");
     inputNumero.value = parseInt(inputNumero.value) + 1;
@@ -12,8 +11,43 @@ function restar2(product2) {
     var inputNumero = document.getElementById("numeroProduct2");
     inputNumero.value = parseInt(inputNumero.value) - 1;
 }
-
 function sumar2(product2) {
     var inputNumero = document.getElementById("numeroProduct2");
+    inputNumero.value = parseInt(inputNumero.value) + 1;
+}
+
+function restar3(product3) {
+    var inputNumero = document.getElementById("numeroProduct3");
+    inputNumero.value = parseInt(inputNumero.value) - 1;
+}
+function sumar3(product3) {
+    var inputNumero = document.getElementById("numeroProduct3");
+    inputNumero.value = parseInt(inputNumero.value) + 1;
+}
+
+function restar4(product4) {
+    var inputNumero = document.getElementById("numeroProduct4");
+    inputNumero.value = parseInt(inputNumero.value) - 1;
+}
+function sumar4(product4) {
+    var inputNumero = document.getElementById("numeroProduct4");
+    inputNumero.value = parseInt(inputNumero.value) + 1;
+}
+
+function restar5(product5) {
+    var inputNumero = document.getElementById("numeroProduct5");
+    inputNumero.value = parseInt(inputNumero.value) - 1;
+}
+function sumar5(product5) {
+    var inputNumero = document.getElementById("numeroProduct5");
+    inputNumero.value = parseInt(inputNumero.value) + 1;
+}
+
+function restar6(product6) {
+    var inputNumero = document.getElementById("numeroProduct6");
+    inputNumero.value = parseInt(inputNumero.value) - 1;
+}
+function sumar6(product6) {
+    var inputNumero = document.getElementById("numeroProduct6");
     inputNumero.value = parseInt(inputNumero.value) + 1;
 }
