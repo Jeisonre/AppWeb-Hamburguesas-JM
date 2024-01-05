54983187e4aff30242bdb912387191d7f8cfa6df
Jeisonre <jeisonespin25@gmail.com>
2024-01-04 22:51:13 -0500

    config

---
+++ articulos.js
@@ -0,0 +1,19 @@
+function restar1(product1) {
+    var inputNumero = document.getElementById("numeroProduct1");
+    inputNumero.value = parseInt(inputNumero.value) - 1;
+}
+
+function sumar1(product1) {
+    var inputNumero = document.getElementById("numeroProduct1");
+    inputNumero.value = parseInt(inputNumero.value) + 1;
+}
+
+function restar2(product2) {
+    var inputNumero = document.getElementById("numeroProduct2");
+    inputNumero.value = parseInt(inputNumero.value) - 1;
+}
+
+function sumar2(product2) {
+    var inputNumero = document.getElementById("numeroProduct2");
+    inputNumero.value = parseInt(inputNumero.value) + 1;
+}
